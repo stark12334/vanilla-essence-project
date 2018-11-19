aeeb884033d68b37da9950000ad7cba80e293a12
stark <starkbryan8@gmail.com>
2018-11-19 10:19:01 +0000

    get method

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,36 @@ let express = require('express');
 let router = express.Router();
 let mongojs = require('mongojs');
 let options = require('../options');
-let url = `mongodb://${options.storageConfig.user}:${options.storageConfig.password}@ds157158.mlab.com:57158/${options.storageConfig.database}`;
-let dbname = options.storageConfig.database;
+let databaseUrl = "mongodb://debo12:debo1234@ds018538.mlab.com:18538/practice-db";
+var collections = ["products_new"];
 
 // storing Database url and collection name 
-let db = mongojs(url,[dbname]);
+var db = require("mongojs")(databaseUrl);
 
 // index page
 router.get('/index',(req,res,next)=>{
-    db.dbname.find((err,dbname)=>{
+    db.products_new.find((err,products_new)=>{
         if(err){
             res.send(err);
         }
-        res.json(dbname);
+        res.json(products_new);
      });
-    console.log(url);
+});
+
+router.post('/index',(req,res,next)=>{
+    // db.products_new.save({
+    //     id: req.id,
+    //     title: req.title,
+    //     picUrl: req.picUrl
+    // },(err,saved)=>{
+    //   if(err || !saved){
+    //       console.log("User not Saved");
+    //   } 
+    //   else{
+    //       console.log(id);
+    //   }
+    // });
+    console.log(req);
 });
 
 module.exports = router;
