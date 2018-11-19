c3f023eac7c26784eadb5dd5618f4fdbabedbd19
stark <starkbryan8@gmail.com>
2018-11-19 14:59:20 +0000

    database authenticated

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,36 @@ let express = require('express');
 let router = express.Router();
 let mongojs = require('mongojs');
 let options = require('../options');
-let databaseUrl = "mongodb://debo12:debo1234@ds018538.mlab.com:18538/practice-db";
-var collections = ["products_new"];
+let databaseUrl = `mongodb://${options.storageConfig.user}:${options.storageConfig.password}@ds157158.mlab.com:57158/${options.storageConfig.database}`;
 var AWS = require('aws-sdk');
 
 var s3 = new AWS.S3();
 
-// storing Database url and collection name 
+// storing Database url
 var db = require("mongojs")(databaseUrl);
 
-// index page
+//lists all the available products
 router.get('/index',(req,res,next)=>{
     db.products_new.find((err,products_new)=>{
         if(err){
-            res.send(err);
+            res.send(databaseUrl);
         }
         res.json(products_new);
      });
 });
 
+// save the product details to DB and s3 bucket as well
 router.post('/index',(req,res,next)=>{
     db.products_new.save({
         title: req.body.title,
         picUrl: req.body.picUrl
-    },(err,saved)=>{
-      if(err || !saved){
+    },(err,products_new)=>{
+      if(err || !products_new){
           console.log("User not Saved");
       } 
       else{
-          sConnect();
+        //   sConnect();
+        console.log('saved');
       }
     });
 });
